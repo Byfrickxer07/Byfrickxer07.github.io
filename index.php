e7918b450d08045165e3f189829157decc1e4840
Byfrickxer07 <byfrickxer@gmail.com>
2023-09-05 20:00:22 +0000

    h

--- index.php
+++ index.php
@@ -3,11 +3,53 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>aviones</title>
+    <link rel="stylesheet" href="style.css">
+    <div class="header2">
+        <h1>Galeria de Aviones de combate</h1>
+    </div>
+
+    <?php  
+
+$mysqli = mysqli_connect('localhost', 'USUphpmyadmin', 'RedesInformaticas', 'db_fricker');
+if (mysqli_connect_errno()) {
+exit('Failed to connect to MySQL: ' . mysqli_connect_error());}
+
+?>
+
+<?php
+		if (!empty($_GET['aviones'])) {
+			$queryavionEspecifico = "SELECT * FROM aviones WHERE id = ". $_GET['aviones'];
+			$resultavionEspecifico = mysqli_query($conexionDatos, $queryavionEspecifico);
+			$avionEspecifico = mysqli_fetch_array($resultavionEspecifico);
+			echo "<title></title>";
+		} else {
+			echo "<title></title>";
+		}
+	?>
+
 </head>
 <body>
-    <h1>multiple select box
-        
-    </h1>
+<form action="index.php">
+   <label for="">Elegí el avion que desea ver::</label>
+  <select name="aviones">
+    <option value="F15">F15</option>
+    <option value="F16">F16</option>
+    <option value="lookhead">lookhead</option>
+     </select>
+  <input type="submit">
+  </form>
+  <?php
+	if (!empty($_GET['avion'])) {
+		echo "<h3>viendo:$avionEspecifico[1]</h3>";
+		$queryfotoavion = "SELECT * FROM aviones WHERE id_foto =  ".$_GET['aviones'];
+		echo "<img>$fotoavion[1]</img>";
+			echo "<a href=\"index.php\">Volver</a>";
+		} else {
+			echo "<br>";
+		}
+		mysqli_close($conexionDatos);
+	?>
+
 </body>
 </html>
